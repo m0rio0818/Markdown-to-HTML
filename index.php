d3cc3095afe0f54aa33c9dee60e94e4091549385
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-03-21 11:24:53 +0900

    tailwindcssをcdnから読み込むように修正

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <div id="editor-container" style="width:800px;height:600px;border:1px solid grey"></div>
+    <div class="flex flex-row">
+        <div id="editor-container" style="width:800px;height:600px;border:1px solid grey"></div>
+        <div id="editor-container1" style="width:800px;height:600px;border:1px solid grey"></div>
+    </div>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/loader.min.js"></script>
 <script>
@@ -20,7 +24,11 @@
     require(['vs/editor/editor.main'], function() {
         let editor = monaco.editor.create(document.getElementById('editor-container'), {
             value: '',
-            language: 'markdown'
+            language: 'md'
+        });
+        let viwer = monaco.editor.create(document.getElementById('editor-container1'), {
+            value: '',
+            language: 'html'
         });
     });
 </script>
