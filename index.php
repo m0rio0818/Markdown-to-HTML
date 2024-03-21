32542e390743d6ccd1f9b2a7b7879e9fe5942a66
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-03-21 12:01:52 +0900

    画面レイアウトを作成

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
 </head>
 
 <body>
-    <div class="flex flex-row">
-        <div id="editor-container" style="width:800px;height:600px;border:1px solid grey"></div>
-        <div id="editor-container1" style="width:800px;height:600px;border:1px solid grey"></div>
+    <div class="flex h-screen flex-col bg-sky-500 lg:flex-row px-5 lg:px-5 py-5 lg:py-2">
+        <div id="editor-container" class="w-full lg:w-1/2  h-1/2 md:h-3/4 lg:h-full" style="border:1px solid grey"></div>
+        <div class="w-full lg:w-1/2 h-1/2 md:h-3/4 lg:h-full" style="border:1px solid grey">
+        <div class="pt-3 flex justify-center">
+            <button class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                Preview
+            </button>
+            <button class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                HTML
+            </button>
+            <button class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                Highlihgt : ON
+            </button>
+            <button class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                Download
+            </button>
+        </div>
+        </div>
     </div>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/loader.min.js"></script>
